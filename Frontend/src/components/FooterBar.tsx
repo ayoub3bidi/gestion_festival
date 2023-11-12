faac973e18c33948ccbd3b7c1b459c4eceba99d8
Ayoub Abidi <mrayoubabidi@gmail.com>
2023-11-12 14:05:09 +0100

    fix: fix footbar

--- Frontend/src/components/FooterBar.tsx
+++ Frontend/src/components/FooterBar.tsx
@@ -1,32 +1,16 @@
-import React, { ReactNode } from 'react'
+import React from 'react'
 import { containerMaxW } from '../config'
-import JustboilLogo from './JustboilLogo'
 
-type Props = {
-  children: ReactNode
-}
-
-export default function FooterBar({ children }: Props) {
-  const year = new Date().getFullYear()
+export default function FooterBar() {
+  // const year = new Date().getFullYear()
 
   return (
     <footer className={`py-2 px-6 ${containerMaxW}`}>
       <div className="block md:flex items-center justify-between">
         <div className="text-center md:text-left mb-6 md:mb-0">
           <b>
-            &copy;{year},{` `}
-            <a href="https://justboil.me/" rel="noreferrer" target="_blank">
-              JustBoil.me
-            </a>
-            .
+            {/* &copy;{year} */}
           </b>
-          {` `}
-          {children}
-        </div>
-        <div className="md:py-2">
-          <a href="https://justboil.me" rel="noreferrer" target="_blank">
-            <JustboilLogo className="w-auto h-8 md:h-6 mx-auto" />
-          </a>
         </div>
       </div>
     </footer>

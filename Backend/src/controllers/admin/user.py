d0df32483d0f9880932b664870ae2e6d1e46dc8c
Ayoub Abidi <mrayoubabidi@gmail.com>
2023-11-19 14:20:14 +0100

    fix: fix user update

--- Backend/src/controllers/admin/user.py
+++ Backend/src/controllers/admin/user.py
@@ -50,6 +50,8 @@ def update_user(user_id, payload, db):
     if payload.disabled != None:
         User.disabled = user.disabled
         user.update({"disabled": payload.disabled})
+        
+    db.commit()
 
     return {
         "message": "user been updated successfully"

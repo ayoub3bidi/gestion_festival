f1fec077e6f0baf70dd7c7f12cdae81851640ac6
Ayoub Abidi <mrayoubabidi@gmail.com>
2023-12-09 17:24:34 +0100

    fix: leave only show for charts endpoints

--- Backend/src/controllers/user/show.py
+++ Backend/src/controllers/user/show.py
@@ -5,9 +5,9 @@ def get_linear_regression_chart_for_shows(payload, db):
     shows = db.query(Show).filter(Show.is_available == True).all()
     return get_linear_regression(shows, payload, "Shows Linear Regression Chart")
 
-def get_kmeans_classification_chart_for_shows(db):
+def get_kmeans_classification_chart_for_shows(payload, db):
     shows = db.query(Show).filter(Show.is_available == True).all()
-    return get_kmeans_classification(shows, "Shows KMeans Classification Chart")
+    return get_kmeans_classification(shows, payload, "Shows KMeans Classification Chart")
 
 def get_decision_tree_chart_for_shows(payload, db):
     shows = db.query(Show).filter(Show.is_available == True).all()
